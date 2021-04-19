5bd9567df266a3520045928960899d2f40b7afaa
Adan Sandoval <adansinacento@gmail.com>
2021-04-18 19:46:39 -0500

    Remove oldest messages

--- Jobsity/Scripts/chat/chat_behavior.js
+++ Jobsity/Scripts/chat/chat_behavior.js
@@ -16,10 +16,15 @@ $(document).ready(function () {
                     .append(
                         $('<small></small>').text(footer).addClass('text-muted')
                     )
-            );
+        );
+
+        var message_holder = $('#msg-list');
 
         //display on screen
-        $('#msg-list').append($div);
+        message_holder.append($div);
+
+        //remove old elements if there are more than 50
+        KeepOnlyNElements(message_holder, 50);
 
         // clear input
         $('#msg-input').val('').focus();
@@ -28,6 +33,13 @@ $(document).ready(function () {
     };
     $.connection.hub.start();
 });
+
+function KeepOnlyNElements(elem, n) {
+    while (elem.children().length > n) { //iterate until children count is the number we want
+        elem.find('div:first').remove(); //on each iteration remove the first child (oldest element)
+    }
+}
+
 
 function ScrollDown() {
     $("html, body").animate({ scrollTop: $(document).height() }, 1000);
